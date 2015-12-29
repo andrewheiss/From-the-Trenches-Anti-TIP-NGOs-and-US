24fd7b412072270747cc2f4b568b7c70e0056bec
Andrew Heiss <andrew@andrewheiss.com>
2015-12-28 22:04:04 -0500

    Create simple summary table

---
+++ R/table_us_opinions.R
@@ -0,0 +1,31 @@
+source(file.path(PROJHOME, "R", "table_functions.R"))
+
+# --------------------------
+# Table for opinions of US
+# --------------------------
+# Initial dataframe
+df <- data_frame(Question = NA, ` ` = NA,
+                 Summary = NA, `Number of responses` = NA)
+
+# Q3.19
+question.id <- "Q3.19"
+var.name <- "Q3.19"
+question <- "Overall, how important a role would you say that the United States or its embassy have played in fighting trafficking in your country over the last 10–15 years?"
+
+df <- bind_rows(df, generate.row.single(responses.full, question.id, 
+                                        question, var.name))
+
+# Q3.25
+question.id <- "Q3.25"
+var.name <- "Q3.25"
+question <- "Overall, has the US influence on human trafficking policy in your country been positive or negative?"
+
+df <- bind_rows(df, generate.row.single(responses.full, question.id, 
+                                        question, var.name))
+
+# Save as Markdown table
+cat(pandoc.table.return(slice(df, 2:n()), split.tables=Inf,
+                        justify=c("left", "center", "left", "center"), 
+                        style="multiline", 
+                        caption="Summary of responses related to opinions of the US {#tbl:us_opinions}"), 
+    file=file.path(PROJHOME, "manuscript", "tables", "summary_us_opinions.md"))

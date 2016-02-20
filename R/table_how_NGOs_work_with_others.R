af311065eb2420246d9a353fa9be634f689bd601
Andrew Heiss <andrew@andrewheiss.com>
2016-02-20 00:07:50 -0500

    Remove old and duplicate questions

--- R/table_how_NGOs_work_with_others.R
+++ R/table_how_NGOs_work_with_others.R
@@ -25,23 +25,6 @@ question <- "In your view, how hard is the government of your country working to
 df <- bind_rows(df, generate.row.single(responses.full, question.id, 
                                         question, var.name))
 
-# Q3.21
-question.id <- "Q3.21"
-question <- "Has your organization used the US State Department’s Trafficking in Persons (TIP) report to discuss trafficking issues with any of these groups?"
-cols <- c("Q3.21_1", "Q3.21_2", "Q3.21_3", "Q3.21_4")
-labels <- c("National government", "Another government", "Other NGOs", "Other")
-
-df <- bind_rows(df, generate.row.mult.responses(responses.full, question.id, 
-                                                question, cols, labels))
-
-# Q3.29
-question.id <- "Q3.29"
-var.name <- "Q3.29"
-question <- "How much is your organization’s work restricted by government regulations in your country?"
-
-df <- bind_rows(df, generate.row.single(responses.full, question.id, 
-                                        question, var.name))
-
 # Save as Markdown table
 cat(pandoc.table.return(slice(df, 2:n()), split.tables=Inf,
                         justify=c("left", "center", "left", "center"), 

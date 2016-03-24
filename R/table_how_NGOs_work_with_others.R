4dd7efa79d7b6a243f1525bfb47dd5781ab1ef98
Andrew Heiss <andrew@andrewheiss.com>
2016-03-24 00:17:21 -0400

    Add summary of government efforts

--- R/table_how_NGOs_work_with_others.R
+++ R/table_how_NGOs_work_with_others.R
@@ -24,6 +24,17 @@ question <- "In your view, how hard is the government of your country working to
 df <- bind_rows(df, generate.row.single(responses.full, question.id, 
                                         question, var.name))
 
+# Q3.26
+question.id <- "Q3.26"
+var.name <- "Q3.26"
+question <- "How have the government's trafficking efforts in your country changed over the past 10–15 years?"
+
+responses.full <- responses.full %>%
+  mutate(Q3.26 = factor(Q3.26, levels=c("Improved", "Remained constant", 
+                                        "Slowed down", "Don't know")))
+df <- bind_rows(df, generate.row.single(responses.full, question.id, 
+                                        question, var.name))
+
 # Save as Markdown table
 cat(pandoc.table.return(slice(df, 2:n()), split.tables=Inf,
                         justify=c("left", "center", "left"), 
